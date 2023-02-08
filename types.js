49c524dca8d084a51c6ce00808b5103ca2e84b68
Dustin Bailey <bailey.dust@gmail.com>
2023-02-08 09:05:42 -0800

    Adds issue plugin & logging for development

--- types.js
+++ types.js
@@ -6,13 +6,33 @@ const SDK = {
     ios:"ios",
     backend:"backend"
 }
+const EVENT_TYPES = {
+    error:'error',
+    transaction:'transaction'
+}
+
+const ISSUE_TYPES = {
+    'release_health.versioning':'release_health.versioning',
+    'release_health.environment.none':'release_health.environment.none',
+    'quota.high':'quota.high',
+    'quita.low':'quota.low',
+    'assignment.some':'assignment.some',
+    'assignment.none':'assignment.none',
+    'ownership.some':'ownership.some',
+    'ownership.none':'ownership.none',
+    'dashboard.none':'dashboard.some',
+    //alerts.metric.none
+    //alerts.issue.none
+    //integrations.vcs.none
+    //integrations.alerting.none
+}
 
 const SDK_GROUP = {
     mobile:['android','ios','cordova'],
     backend:['python','java','go']
 }
 
-const DEPENDENCIES = {
+const DEPENDENCY_TYPES = {
     sdk:'sdk',
     event:'event',
     issue:'issue',
@@ -28,7 +48,7 @@ const rawRules = require('./rules.json');
 // class Rule {
 //     /**
 //      * @param {string} body The outbound text
-//      * @param {Object} deps The dependencies of the outbound)
+//      * @param {Object} deps The DEPENDENCY_TYPES of the outbound)
 //      * @param {int} priority The integer priority 0 ... 10
 //      */
 
@@ -76,7 +96,7 @@ class Engine {
         let output = [];
         for(const r of this.ruleSet){
             console.log(r.deps);
-            if(this.applyPlugins(this.plugins,r.deps) == true){
+            if(this.applyPlugins(this.plugins,r.deps) === true){
                 //add rule to some output w/priority
                 output.push(r);
             }
@@ -89,12 +109,13 @@ class Engine {
        
         // any applicable plugins that return false violate dependency reqs
         // perhaps register plugins as check agains missing (if rules specify)
-        let result = false;
+        let result = true;
         for (let plugin of pluginArray){
-         
+            //this leaves the potential for some rules to not be applied if plugin is missing from [Plugins]
             if(plugin.dependency in ruleDeps){
-               result = plugin.evaluate(this.accountData,ruleDeps[plugin.dependency]);
+               result = result && plugin.evaluate(this.accountData,ruleDeps[plugin.dependency]);
                //perhaps plugin can ve stateless meh we need state
+               console.log('result',result);
             }
             
         }
@@ -103,15 +124,49 @@ class Engine {
     }
 }
 
-class Plugin {
+
+class IssuePlugin {
+    
+    constructor(){
+        this.dependency = DEPENDENCY_TYPES.issue;
+    }
+   
+    evaluate(accountData, issueDepsArray){
+        /**
+         * 
+         * @return {boolean} Returns true or false if dependency present
+         */
+        console.log("applying plugin",this.dependency,accountData,issueDepsArray);
+
+        //what api do I need from account data to evaluate each type of issue? A really long switch 
+        //My deps array dictates the issues?
+        let result = false;
+        //For each in issue rule, query the data api to assess
+        for (let issueType of issueDepsArray){
+           console.log('issueType',issueType)
+            if (issueType === ISSUE_TYPES["dashboard.none"]){
+
+            }
+            else if (issueType === ISSUE_TYPES["release_health.environment.none"]){
+                
+                if(!accountData.hasEnv()) result = true;
+            }
+            else if(issueType === ISSUE_TYPES["ownership.none"]){
+               
+                if (!accountData.hasOwnership()) result = true;
+            }
+            else throw new Error(`Issue plugin did not find matching Issue dep: ${issueType}`);
+        }
+        return result
+    }
+}
+
+class SdkPlugin {
     /**
      * @return {boolean} Returns true or false if dependency present
      */
-}
-
-class SDKPLUGIN {
     constructor(){
-        this.dependency = DEPENDENCIES.sdk;
+        this.dependency = DEPENDENCY_TYPES.sdk;
     }
     //if it is mobile use the superset
     //else go through 
@@ -119,9 +174,13 @@ class SDKPLUGIN {
 
 
    evaluate(accountData,ruleDepsArray){
+       /**
+        * 
+        * @return {boolean} Returns true or false if dependency present
+        */
     //check set equivalence or subset
     //assumes no dupes
-    console.log("applying plugin",accountData,ruleDepsArray)
+    console.log("applying plugin",this.dependency,accountData,ruleDepsArray)
     
     let expandedSet = new Set();
 
@@ -146,8 +205,9 @@ class SDKPLUGIN {
     });
 
     const a = Array.from(expandedSet);
-    
-    if(!a.length ===0 && emisSubset(accountSdks,a)){
+    console.log('expanded set',a);
+    if(a.length !== 0 && isSubset(accountSdks,a)){
+        console.log('returning true for expanded set')
         return true;
     }
     return false
@@ -164,9 +224,11 @@ function isSubset(dep1,dep2){
 }
 
 const mockAccount = {
-    getSdks:() => ['android','javascript','java']
+    getSdks:() => ['android','javascript','java'],
+    hasOwnership:() => false,
+    hasEnv:() => false
 }
-const plugins = [new SDKPLUGIN()]; //if plugin isn't defined but rule is present this should throw
+const plugins = [new SdkPlugin(), new IssuePlugin()]; //if plugin isn't defined but rule is present this should throw
 const e = new Engine(rawRules,mockAccount,plugins);
 console.log("outbound output", e.process());
 
